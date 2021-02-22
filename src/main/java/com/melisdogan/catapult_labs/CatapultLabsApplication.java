9b37f5a33b553689c0d9441953593a5f3d0eee70
melis <melis@melisdogan.com>
2021-02-22 20:15:31 +0300

    init

---
+++ src/main/java/com/melisdogan/catapult_labs/CatapultLabsApplication.java
@@ -0,0 +1,13 @@
+package com.melisdogan.catapult_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatapultLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatapultLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melisdogan/catapult_labs/CatapultLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melisdogan.catapult_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatapultLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
